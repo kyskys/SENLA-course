d49c60f6e28b1eb5f7a769a1ad2600ac00140ab5
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-09-18 10:43:42 +0300

    задание 3

--- task 3/src/product/IProduct.java
+++ task 3/src/product/IProduct.java
@@ -3,6 +3,7 @@ package product;
 import part.IProductPart;
 
 public interface IProduct {
+	
 	public void installFirstPart(IProductPart p);
 
 	public void installSecondPart(IProductPart p);
--- task 3/src/product/Pencil.java
+++ task 3/src/product/Pencil.java
@@ -2,23 +2,27 @@ package product;
 
 import part.*;
 
-public class Pencil implements IProduct{
+public class Pencil implements IProduct {
+
 	public IProductPart firstPart;
 	public IProductPart secondPart;
 	public IProductPart thirdPart;
+
 	@Override
 	public void installFirstPart(IProductPart p) {
-		firstPart=p;
+		firstPart = p;
 		System.out.println("first part installed");
 	}
+
 	@Override
 	public void installSecondPart(IProductPart p) {
-		secondPart=p;
+		secondPart = p;
 		System.out.println("second part installed");
 	}
+
 	@Override
 	public void installThirdPart(IProductPart p) {
-		thirdPart=p;
+		thirdPart = p;
 		System.out.println("third part installed");
 	}
 }
