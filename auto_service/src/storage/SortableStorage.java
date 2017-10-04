316002988b67d0d475f9d74ccaf97c59dfe6f214
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-10-04 14:59:27 +0300

    добавил uml диаграмму для автосервиса

--- auto_service/src/storage/SortableStorage.java
+++ auto_service/src/storage/SortableStorage.java
@@ -9,6 +9,7 @@ import storage.interfaces.ISortableStorage;
 public abstract class SortableStorage<T extends BaseEntity> extends AbstractStorage<T> implements ISortableStorage<T> {
 	protected abstract void sort(List<T> listToSort, SortParameters parameter);
 
+	@Override
 	public List<T> getAll(SortParameters parameter) {
 		sort(list, parameter);
 		return list;
