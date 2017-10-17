8b0dcef94251365f21f6a67c57573355defa8c3c
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-10-17 17:19:47 +0300

    переделал загрузчика конфигурации

--- configuration/src/loader/ConfigLoader.java
+++ configuration/src/loader/ConfigLoader.java
@@ -1,29 +1,20 @@
 package loader;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class ConfigLoader {
-	Path configPath;
-
-	public ConfigLoader() throws IOException {
-		Path configPath = Paths.get("properties.config");
-		if (!Files.exists(configPath)) {
-			Files.createFile(configPath);
-		}
-	}
 
 	public Map<Object, Object> load() throws IOException {
 		Map<Object, Object> result = new HashMap<Object, Object>();
-		List<String> lines = Files.readAllLines(configPath);
-		for (String line : lines) {
-			String[] property = line.split(":");
-			result.put(property[0], property[1]);
+		Properties props = new Properties();
+		FileInputStream fis = new FileInputStream("config.properties");
+		props.load(fis);
+		for (String key : props.stringPropertyNames()) {
+			result.put(key, props.getProperty(key));
 		}
 		return result;
 	}
