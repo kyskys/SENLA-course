d4d2d263e4a80a48c62805dd592282b050c5fe83
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-10-27 17:46:22 +0300

    сделал DI

--- dependency/src/dependency/DependencyManager.java
+++ dependency/src/dependency/DependencyManager.java
@@ -1,39 +1,37 @@
 package dependency;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 public class DependencyManager {
 	static Map<Object, Object> instanceMap = new HashMap<Object, Object>();
-	{
-		try {
-			initMap();
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IOException e) {
-			e.printStackTrace();
-		}
+
+	static {
+		initMap();
 	}
 
-	public static void initMap()
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+	public static void initMap() {
 		loadMapForPath("resources/configStorage.properties");
 		loadMapForPath("resources/configService.properties");
+		loadMapForPath("resources/system.properties");
+
 	}
 
-	private static void loadMapForPath(String path)
-			throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+	private static void loadMapForPath(String path) {
 		try (FileInputStream fis = new FileInputStream(path);) {
 			Properties p = new Properties();
 			p.load(fis);
 			for (String key : p.stringPropertyNames()) {
 				instanceMap.put(Class.forName(key), Class.forName(p.getProperty(key)).newInstance());
 			}
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")	
 	public static <T> T getInstance(Class<T> c) {
 		return (T) instanceMap.get(c);
 	}
